843acaac76ce7b54633652c18a1d31d978f52bdf
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-02-05 15:11:24 +0500

    The theme work is done for login/register & delete pages

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,7 @@
 from dataclasses import field
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -8,3 +9,9 @@ class RoomForm(ModelForm):
         fields = '__all__'
         exclude = ['host' ,'participants']
 
+class UserForm(ModelForm):
+    class Meta:
+         model = User
+         fields = ['username' , 'email']
+
+  
