69f3087ba33673e29b9d13590efafefeb988f35f
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-02-08 01:11:14 +0500

    Deployment commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--h=#sp@%lbrww^cd20$qbp40ji70=cm=izn%7pkzv0hl4ev-nv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['groupper.herokuapp.com','127.0.0.1']
 
 
 # Application definition
