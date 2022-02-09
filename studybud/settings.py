f766c4a58c99b52c8bd9ac1a4a423a4a13f43a78
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-02-09 17:04:19 +0500

    settings update

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--h=#sp@%lbrww^cd20$qbp40ji70=cm=izn%7pkzv0hl4ev-nv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['groupper-platform.herokuapp.com','127.0.0.1']
 
