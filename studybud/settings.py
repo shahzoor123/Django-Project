d7afeadfc54633c111ebef9ddb42be3c3c923fa3
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-02-09 16:14:26 +0500

    settings update

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--h=#sp@%lbrww^cd20$qbp40ji70=cm=izn%7pkzv0hl4ev-nv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['groupper-platform.herokuapp','127.0.0.1']
+ALLOWED_HOSTS = ['groupper-platform.herokuapp','127.0.0.1']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
