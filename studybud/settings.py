3500118d9cc587c06cb2b81d9f270a8bfe1b8d2b
shahzor khan <lamborghinia693@gmail.com>
2022-04-11 23:58:33 -0700

    new models

--- studybud/settings.py
+++ studybud/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT =  BASE_DIR / 'static/images'
 
 # MEDIA_ROOT = BASE_DIR, 'static/images'
 
